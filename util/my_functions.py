a07846e9b7be67a6283f822dadd103af670c891b
Hyem <hiddenmaze91@gmail.com>
2018-02-23 04:30:55 +0900

    Update Metadata Loader

--- util/my_functions.py
+++ util/my_functions.py
@@ -3,6 +3,7 @@ from os.path import isfile, join
 from google_drive_downloader import GoogleDriveDownloader as gdd
 import gzip
 import gensim
+import numpy as np
 
 def load_w2v(w2v_path):
     if isfile(w2v_path) == False:
@@ -19,5 +20,19 @@ def load_w2v(w2v_path):
         remove(w2v_path+'.gz')
 
     w2v_model = gensim.models.KeyedVectors.load_word2vec_format(w2v_path, binary=True)
-    
     return w2v_model
+
+def load_metadata(metadata_path):
+    if isfile(metadata_path) == False:
+        print "Start downloading preprocessed metadata"
+        gdd.download_file_from_google_drive(file_id='1k3FJOYslo7PU3U4TyM3VFuiIgpcxMEjZ',
+                                            dest_path=metadata_path,
+                                            unzip=False)
+    npzfile = np.load(metadata_path)
+
+    train_action = npzfile['arr_0']
+    train_script = npzfile['arr_1']
+    train_length = npzfile['arr_2']
+    sentence_steps = np.int(npzfile['arr_3'])    
+    
+    return train_action, train_script, train_length, sentence_steps  
